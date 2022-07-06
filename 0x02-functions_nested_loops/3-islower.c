20badc57338a60b1eff1e8c65f6dc54acf64e724
JESMEX <str82jes@gmail.com>
2022-07-06 06:21:53 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,10 @@
 
 /**
 * islowercase -> checks for lowercase character
-* @c: character argument
+* @c: character argumenit
 * Return: returns 1 and 0 depending on the condition
 */
-int _islower(int c);
+int_islower(int c);
 {
 return (c >= 'a' && c <= 'z');
 }

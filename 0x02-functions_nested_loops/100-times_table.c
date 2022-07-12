61e5d71af732db5fbe21316dc8dcb438f6296e21
JESMEX <str82jes@gmail.com>
2022-07-11 17:36:51 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,9 +16,7 @@ for (j = 0; j <= n; j++)
 {
 k = j * i;
 if (j == 0)
-{
-_putchar(k + '0');
-}
+{_putchar(k + '0'); }
 else if (k < 10 && j != 0)
 {
 _putchar(',');
@@ -41,10 +39,6 @@ _putchar(',');
 _putchar(' ');
 _putchar((k / 100) + '0');
 _putchar(((k / 10) % 10) + '0');
-_putchar((k % 10) + '0');
-}
-}
-_putchar('\n');
-}
-}
+_putchar((k % 10) + '0'); }}
+_putchar('\n'); }}
 }
